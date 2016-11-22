25fa7fb782797e5b3ce8e3550f2099f4740be947
Code Monkey <jkotarac@extensionengine.com>
2016-11-22 11:50:09 +0100

    cleaned edx-notification code

--- gradebook/signals.py
+++ gradebook/signals.py
@@ -11,12 +11,6 @@ from django.db.models.signals import post_save, pre_save
 from courseware.signals import score_changed
 from util.signals import course_deleted
 from student.roles import get_aggregate_exclusion_user_ids
-from edx_notifications.lib.publisher import (
-    publish_notification_to_user,
-    get_notification_type
-)
-from edx_notifications.data import NotificationMessage
-
 
 from gradebook.models import StudentGradebook, StudentGradebookHistory
 from gradebook.tasks import update_user_gradebook
@@ -92,34 +86,4 @@ def handle_studentgradebook_post_save_signal(sender, instance, **kwargs):
         if grade > 0.0:
             leaderboard_size = getattr(settings, 'LEADERBOARD_SIZE', 3)
             presave_leaderboard_rank = instance.presave_leaderboard_rank if instance.presave_leaderboard_rank else sys.maxint
-            if leaderboard_rank <= leaderboard_size and presave_leaderboard_rank > leaderboard_size:
-                try:
-                    notification_msg = NotificationMessage(
-                        msg_type=get_notification_type(u'open-edx.lms.leaderboard.gradebook.rank-changed'),
-                        namespace=unicode(instance.course_id),
-                        payload={
-                            '_schema_version': '1',
-                            'rank': leaderboard_rank,
-                            'leaderboard_name': 'Proficiency',
-                        }
-                    )
 
-                    #
-                    # add in all the context parameters we'll need to
-                    # generate a URL back to the website that will
-                    # present the new course announcement
-                    #
-                    # IMPORTANT: This can be changed to msg.add_click_link() if we
-                    # have a particular URL that we wish to use. In the initial use case,
-                    # we need to make the link point to a different front end website
-                    # so we need to resolve these links at dispatch time
-                    #
-                    notification_msg.add_click_link_params({
-                        'course_id': unicode(instance.course_id),
-                    })
-
-                    publish_notification_to_user(int(instance.user.id), notification_msg)
-                except Exception, ex:
-                    # Notifications are never critical, so we don't want to disrupt any
-                    # other logic processing. So log and continue.
-                    log.exception(ex)
